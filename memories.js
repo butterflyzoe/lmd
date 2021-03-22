b4ba984629f0d285e44b0ecf167cb73fdb324aca
butterflyzoe <70614912+butterflyzoe@users.noreply.github.com>
2021-03-23 02:52:59 +0800

    Add files via upload

---
+++ memories.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('#dowebok').fullpage({
+        'navigation': true,
+        'navigationPosition':"left",
+        'navigationColor':['#fff'],
+    });
+});
+loadAudioFile('../music/易烊千玺 - 宝贝.mp3');
